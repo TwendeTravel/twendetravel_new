b694fa860dc17dcf32988890d1f432c0bd2c126b
Chrys-13 <elikem.gale-zoyiku@ashesi.edu.gh>
2025-06-10 16:44:02 +0000

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,13 @@ import type { Config } from "tailwindcss";
 export default {
 	darkMode: ["class"],
 	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./src/**/*.{vue,html,md,mdx}",
+		"./src/**/*.{astro,html,js,jsx,ts,tsx}",
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
 	],
 	prefix: "",
 	theme: {
